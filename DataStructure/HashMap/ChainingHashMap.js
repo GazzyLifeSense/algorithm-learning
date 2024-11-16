5eaafbb555e029f12ffc997daae75ed48b8bcd44
lotusFoundAnotherWorld <2636258590@qq.com>
2024-11-16 10:51:34 +0800

    ChaningHashMap

---
+++ DataStructure/HashMap/ChainingHashMap.js
@@ -0,0 +1,165 @@
+/*
+ * 拉链法实现哈希表
+ *
+ * Ⅰ使用单链表节点，存储 key-value 对
+ */
+import { SingleLinkedList } from '../LinkedList/SingleLinkedList'
+class KVNode{
+    constructor(key, value) {
+        this.key = key
+        this.value = value
+    }
+}
+class ChainingHashMap {
+    constructor(initCapacity) {
+        // 哈希表的底层数组，每个数组元素是一个链表，链表中每个节点是 KVNode 存储键值对
+        this.table = []
+        // 哈希表中存入的键值对个数
+        this.size = 0
+        // 底层数组的初始容量
+        this.INIT_CAP = 4
+
+        this.init(initCapacity)
+    }
+    
+    // 初始化哈希表
+    init(initCapacity) {
+        const capacity = initCapacity || this.INIT_CAP
+        this.table = new Array(capacity)
+        this.size = 0
+        for (let i = 0; i < capacity; i++){
+            this.table[i] = new SingleLinkedList()
+        }
+    }
+
+    /***** 增/改 *****/
+
+    // 添加 key -> val 键值对
+    // 如果键 key 已存在，则将值修改为 val
+    put(key, val) {
+        if (key == null) {
+            throw new Error("key is null");
+        }
+
+        const list = this.table[this.hash(key)];
+        // 如果 key 之前存在，则修改对应的 val
+        for (let node of list) {
+            if (node.key === key) {
+                node.value = val;
+                return;
+            }
+        }
+        // 如果 key 之前不存在，则插入链表头部，size 增加
+        list.addFirst(new KVNode(key, val));
+        this.size++;
+
+        // 如果元素数量超过了负载因子，进行扩容
+        if (this.size >= this.table.length * 0.75) {
+            this.resize(this.table.length * 2);
+        }
+    }
+
+    /***** 删 *****/
+
+    // 删除 key 和对应的 val
+    remove(key) {
+        if (key == null) {
+            throw new Error("key is null");
+        }
+
+        const list = this.table[this.hash(key)];
+        // 如果 key 存在，则删除，size 减少
+        for (let i = 0; i < list.size; i++) {
+            if (list.get(i).key === key) {
+                list.remove(i);
+                this.size--;
+
+                // 缩容，当负载因子小于 0.125 时，缩容
+                if (this.size <= this.table.length / 8) {
+                    this.resize(Math.floor(this.table.length / 4));
+                }
+                return;
+            }
+        }
+    }
+
+    /***** 查 *****/
+
+    // 返回 key 对应的 val，如果 key 不存在，则返回 null
+    get(key) {
+        if (key == null) {
+            throw new Error("key is null");
+        }
+
+        const list = this.table[this.hash(key)];
+        for (let node of list) {
+            if (node.key === key) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    // 返回所有 key
+    keys() {
+        keys = [];
+        for (let list of this.table) {
+            for (let node of list) {
+                keys.push(node.key);
+            }
+        }
+        return keys;
+    }
+
+    /***** 其他工具函数 *****/
+
+    // 哈希函数
+    hash(key) {
+        const str = JSON.stringify(key)
+        let hash = 0, chr
+        for (let i = 0; i < str.length; i++){
+            chr = str.charCodeAt(i)
+            hash = ((hash << 5) - hash) + chr
+            hash |= 0 // Convert to int
+        }
+        // 映射到 table 的索引
+        hash = (hash & 0x7fffffff) % this.table.length
+        
+        return hash;
+    }
+
+    resize(newCap) {
+        // 构造一个更大容量的 HashMap
+        const newMap = new ChainingHashMap(newCap);
+        // 穷举当前 HashMap 中的所有键值对
+        for (let list of this.table) {
+            for (let node of list) {
+                // 将键值对转移到新的 HashMap 中
+                newMap.put(node.key, node.value);
+            }
+        }
+        // 将当前 HashMap 的底层 table 换掉
+        this.table = newMap.table;
+    }
+
+    
+}
+
+/* TEST CODE */
+function test() { 
+    const map = new ChainingHashMap();
+    map.put(1, 1);
+    map.put(2, 2);
+    map.put(3, 3);
+    console.log(map.get(1)); // 1
+    console.log(map.get(2)); // 2
+
+    map.put(1, 100);
+    console.log(map.get(1)); // 100
+
+    map.remove(2);
+    console.log(map.get(2)); // null
+
+    console.log(map.keys()); // [1, 3]（顺序可能不同）
+}
+test()
---
+++ DataStructure/HashMap/HashMap.js
@@ -0,0 +1,50 @@
+/*
+ * 哈希表
+ *
+ * Ⅰ 可以理解为加强版的数组
+ * 
+ * Ⅱ 根据key查找value的时间复杂度O(1)，受哈希函数时间复杂度和哈希冲突影响
+ *
+ * Ⅲ key 唯一，value 可以重复；key不可变，可以是数字、字符串等多种类型
+ * 
+ * Ⅳ 哈希函数：把任意长度的输入（key）转化成固定长度的输出（索引）
+ *      输入相同key -> 相同输出
+ *      依赖table.length，即扩缩容可能导致哈希值、索引变化
+ * 
+ * Ⅴ 哈希冲突（不可避免）：不同key得到相同输出
+ *      解决方法：
+ *          ①拉链法：纵向延伸，底层数组存储链表，新冲突kv插入链表头部
+ *          ②线性探查法（开放寻址法）：横向延伸，当前index已被占用则往index+1,...查看直到找到空位
+ * 
+ * Ⅵ 负载因子：哈希表装满的程度的度量，当哈希表内元素达到负载因子时，哈希表会扩容。
+ *          计算：size / table.length，默认0.75为佳
+ */
+class HashMap {
+    constructor() {
+        this.table = new Array(1000).fill(null);
+    }
+
+    // 增/改，复杂度 O(1)
+    put(key, value) {
+        const index = this.hash(key);
+        this.table[index] = value;
+    }
+
+    // 查，复杂度 O(1)
+    get(key) {
+        const index = this.hash(key);
+        return this.table[index];
+    }
+
+    // 删，复杂度 O(1)
+    remove(key) {
+        const index = this.hash(key);
+        this.table[index] = null;
+    }
+
+    // 哈希函数，把 key 转化成 table 中的合法索引
+    // 时间复杂度必须是 O(1)，才能保证上述方法的复杂度都是 O(1)
+    hash(key) {
+        // ...
+    }
+}
