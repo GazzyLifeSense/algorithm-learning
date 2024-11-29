f4dcc8175b4a002b7525b49f2be513f7b3307cd1
lotusFoundAnotherWorld <2636258590@qq.com>
2024-11-29 15:10:07 +0800

    [BFS]SlidingPuzzle

--- Algorithms/BruteForceSearch/BFS.js
+++ Algorithms/BruteForceSearch/BFS.js
@@ -156,5 +156,87 @@ var openLockDoubleBFS = function(deadends, target) {
     }
     return -1;
 };
-console.log(openLockDoubleBFS(["0201", "0101", "0102", "1212", "2002"], "0202"))
-console.log(openLockDoubleBFS(["0000"], "8888"))
+// console.log(openLockDoubleBFS(["0201", "0101", "0102", "1212", "2002"], "0202"))
+// console.log(openLockDoubleBFS(["0000"], "8888"))
+
+// 例 773.滑动谜题  https://leetcode.cn/problems/sliding-puzzle（要点在于将多维打平为一维表示，再根据它们的关系进行扩散，并使用BFS暴力解法）
+// 生成邻居索引数组
+function generateNeighbors(row, col) {
+    const NArr = new Array(row * col).fill(0).map(()=>[])
+    for (let i = 0; i < row * col; i++){
+        // 非第一列，有左节点
+        if(i % col != 0) NArr[i].push(i-1)
+        // 非最后一列，有右节点
+        if(i % col != col - 1) NArr[i].push(i+1)
+        // 非第一行，有上节点
+        if(i >= col) NArr[i].push(i - col)
+        // 非最后一行，有下节点
+        if(i < row * col - col) NArr[i].push(i + col)
+    }
+    return NArr
+}
+// 交换两个索引处的字符并返回新字符串
+function swap(str, indexNeighbor, index0) {
+    let chars = str.split('');
+    [chars[indexNeighbor], chars[index0]] = [chars[index0], chars[indexNeighbor]]
+    return chars.join('')
+}
+// 将多维数组打成字符串便于操作
+var slidingPuzzle = function (row = 2, col = 3, board = [[4,1,2],[5,0,3]], target = "123450") {
+    if (row * col != target.length) throw new Error('维度不匹配')
+    
+    // 初始排列序列
+    let start = ""
+    const NArr = generateNeighbors(row, col)
+    for (let i = 0; i < row; i++){
+        for (let j = 0; j < col; j++){
+            start += board[i][j]
+        }
+    }
+
+    console.log('生成多维数组的邻居索引数组：', NArr)
+    console.log('将多维数组打平为一维字符串：', start)
+
+    // 核心数据结构
+    const q = [];
+    // 避免走回头路
+    const visited = new Set();
+    let step = 0;
+    
+    // 将起点加入队列
+    q.push(start);
+    visited.add(start);
+
+    while (q.length > 0) {
+        let sz = q.length;
+        // 将当前队列中的所有排序序列向四周扩散
+        for (let i = 0; i < sz; i++) {
+            let cur = q.shift();
+            // 划重点：这里判断是否到达终点
+            if (cur == target)
+                return step;
+
+            // 找到0的索引
+            let index0
+            for (let i = 0; i < cur.length; i++){
+                if(cur[i] === '0') index0 = i
+            }
+            if (index0 == null) return -1
+            
+            // 将0往相邻索引移动后的新字符串加入队列
+            const Neighbor = NArr[index0]
+            for (var j = 0; j < Neighbor.length; j++) {
+                var newStr = swap(cur, Neighbor[j], index0)
+                if (!visited.has(newStr)) {
+                    console.log('步数：', step+1,  '新串：', newStr)
+                    q.push(newStr);
+                    visited.add(newStr);
+                }
+            }
+        }
+        step++;
+    }
+    // 如果走到这里，说明没有解
+    return -1
+}
+// console.log('达成结果滑动步数: ', slidingPuzzle(3,3,[[4,1,2],[5,0,3],[7,8,9]], '107954238'))
