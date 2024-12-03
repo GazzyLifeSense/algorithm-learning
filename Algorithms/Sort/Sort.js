c3b3079d3f30a6faba58a7307e0b483632481755
LotusFoundAnotherWorld <2636258590@qq.com>
2024-12-03 21:10:07 +0800

    quickSort refactor

--- Algorithms/Sort/Sort.js
+++ Algorithms/Sort/Sort.js
@@ -7,7 +7,9 @@
  *    冒泡排序（基于选择排序优化）: 每个元素根据自身大小一点一点向数组的一侧移动。 （最坏O(n²) 有序可提前终止 原地排序 稳定性）
  *    插入排序（基于选择排序优化）: 通过构建有序序列，将未排序的数据逐个插入到已排序的序列中。 （最坏O(n²) 有序接近O(n) 综合性能优于冒泡 原地排序 稳定性）
  *    希尔排序（基于插入排序优化）: 先对数组进行排序使之成为"h-有序"的，即数组中任意相距h的元素都是有序的，之后再减小h值再次排序，
- *              直至h减小到1成为插入排序，此时数组已部分有序，所以速度会快很多。（最坏O(n^1.5) 原地排序 稳定性）
+ *              直至h减小到1成为插入排序，此时数组已部分有序，所以速度会快很多。（最坏O(n^1.5) 原地排序 不稳定性[相同数字在不同子数组中进行交互位置时相对位置可能发生变化]）
+ *    快速排序（分治）: 在一个无序的序列中选取一个任意的基准元素pivot，利用pivot将待排序的序列分成两部分，前面部分元素均小于或等于基准元素，后面部分均大于或等于基准元素，
+ *              然后采用递归的方法分别对前后两部分重复上述操作，直到将无序序列排列成有序序列。（平均O(nlog2n) 最坏O(n²) 原地排序 不稳定性[121->112], 元素交换时相对位置可能发生变化）
  */
 
 //#region 选择排序: 每次都去遍历选择最小的元素。
@@ -116,48 +118,42 @@ function shellSort(nums) {
 // console.log(shellSort([1,3,0,7,6,5,1,2,3,2]))
 //#endregion
 
-//#region 快速排序: 妙用二叉树前序位置，（递归）
+//#region 快速排序: 在一个无序的序列中选取一个任意的基准元素pivot，利用pivot将待排序的序列分成两部分，前面部分元素均小于或等于基准元素，后面部分均大于或等于基准元素，
+//                 然后采用递归的方法分别对前后两部分重复上述操作，直到将无序序列排列成有序序列。
+// （递归法）妙用二叉树前序位置，在二叉树遍历的前序位置将一个元素排好位置，然后递归地将剩下的元素排好位置。
 function quickSort(nums) {
     
-    function sort(nums, lo, hi) {
-        if (lo >= hi) return
+    function sort(nums, left, right) {
+        if (left >= right) return
         
         // 获取切分点
-        const p = partition(nums, lo, hi)
+        const p = partition(nums, left, right)
 
         // 对左右侧数组进行排序
-        sort(nums, lo, p - 1)
-
-        sort(nums, p + 1, hi)
+        sort(nums, left, p - 1)
+        sort(nums, p + 1, right)
     }
 
-    // 获取支点索引，将小于其的节点放左侧、大于其的节点放右侧
-    function partition(nums, lo, hi) {
-        // 支点
-        const pivot = nums[lo]
-        let i = lo, j = hi
-        while (i <= j) {
-            // 找到值大于支点的索引
-            while (i < hi && nums[i] <= pivot) i++
-            // 找到值小于等于支点的索引
-            while (j > lo && nums[j] > pivot) j--
-            if (i >= j) break
-            [nums[i], nums[j]] = [nums[j], nums[i]]
+    // 核心：分区，将小于支点的节点放左侧、大于支点的节点放右侧，返回支点索引
+    function partition(nums, left, right) {
+        // 支点索引
+        const key = left
+        while (left < right) {
+            // 右边先走保证重合时停留在比key小处
+            // 右边开始寻找值 <= 支点的索引
+            while (left < right && nums[right] >= nums[key]) right--
+            // 左边开始寻找值 > 支点的索引
+            while (left < right && nums[left] <= nums[key]) left++
+            // 左右下标互换
+            [nums[left], nums[right]] = [nums[right], nums[left]]
         }
-        [nums[lo], nums[j]] = [nums[j], nums[lo]]
-        return j
+        // 最终右下标一定是小于等于支点的，所以将支点与左下标交换位置，此时左侧小于支点，右侧大于支点
+        [nums[key], nums[right]] = [nums[right], nums[key]]
+        // 返回支点索引
+        return right
     }
     sort(nums, 0, nums.length - 1)
     return nums
 }
-//     sort(130765) p=1
-//       /      \
-//     [0]   sort(3765) p=0
-//             /     \
-//            []    sort(756)
-//                    \
-//                  sort(56)
-//                    /  \
-//                   []  [6]
-console.log(quickSort([1,3,0,7,6,5]))
+// console.log(quickSort([3,3,5,10,6,8,9,3,1,4]))
 //#endregion
